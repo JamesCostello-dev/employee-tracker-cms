388303ed5fd605122ba74a07226f82045de2a462
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-12-20 13:38:43 -0700

    connect to db

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-const main = require('./db/database');
+const sql = require('mysql2');
+const inq = require('inquirer');
+const tab = require('console.table');
 
+// connect to db
+const connection = sql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'EmployeeTrackerDB'
+});
+
+// connect and log
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  run();
+});
