9c566a5967b92c23d040a5f4301c130b32341ce6
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-12-20 14:11:13 -0700

    inq prompts

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const sql = require('mysql2');
 const inq = require('inquirer');
-const tab = require('console.table');
 
 // connect to db
 const connection = sql.createConnection({
@@ -17,3 +16,81 @@ connection.connect(err => {
   console.log('connected as id ' + connection.threadId);
   run();
 });
+
+const opt = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role'];
+
+// prompt
+const run = async () => {
+  const x = await inq.prompt([
+    {
+      type: 'list',
+      message: 'View options and make a selection',
+      name: 'option',
+      choices: opt
+    },
+  ]);
+  switch (x.option) {
+    case 'View all departments':
+      getDepartments();
+      break;
+    case 'View all roles':
+      getRoles();
+      break;
+    case 'View all employees':
+      getEmployees();
+      break;
+    case 'Add a department':
+      addDepartment();
+      break;
+    case 'Add a Role':
+      addRole();
+      break;
+    case 'Add an employee':
+      addEmployee();
+      break;
+    case 'Update employee role':
+      updateRole();
+  }
+  ;
+};
+
+
+const getDepartments = () => {
+  connection.query('SELECT * FROM department', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    run();
+  });
+};
+
+const getRoles = () => {
+  connection.query('SELECT * FROM role', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    run();
+  });
+};
+
+const getEmployees = () => {
+  connection.query('SELECT * FROM employee', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    run();
+  });
+};
+
+// const addDepartment = () => {
+
+// }
+
+// const addRole = () => {
+
+// }
+
+// const addEmployee = () => {
+
+// }
+
+// const updateRole = () => {
+
+// }
